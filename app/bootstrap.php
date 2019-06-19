de9fba5e85033bb49264deb8d3ba8c076afa8bcf
Daniel Paz-Horta <dpaz@uic.edu>
2019-06-18 22:14:23 -0500

    Remove class use and add try/catch to avoid composer errors on install/update

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -6,18 +6,27 @@
  * Time: 11:19 AM
  */
 
-// Load from within this Package
-use Dotenv\Dotenv;
+try {
 
-$dotenv = Dotenv\Dotenv::create(__DIR__ . '/../');
-$dotenv->load();
+    // Load from within this Package
+    $dotenv = Dotenv\Dotenv::create(__DIR__ . '/../');
+    $dotenv->load();
 
-if(is_readable(__DIR__ . '/../../../../.env')) {
+    if(is_readable(__DIR__ . '/../../../../.env')) {
 
-    // Try to load from the root of of a project that is using this package
-    $dotenv = Dotenv\Dotenv::create(__DIR__ . '/../../../../');
-    $dotenv->overload();
+        // Try to load from the root of of a project that is using this package
+        $dotenv = Dotenv\Dotenv::create(__DIR__ . '/../../../../');
+        $dotenv->overload();
+
+    }
+
+    $dotenv->required(['AITS_STUDENT_ENROLLMENT_APP_DATA_LOG_RELATIVE_PATH', 'AITS_STUDENT_ENROLLMENT_DATA_LOG_FILE_PREFIX', 'AITS_STUDENT_ENROLLMENT_AITS_SENDER_APP_ID', 'AITS_STUDENT_ENROLLMENT_AITS_SERVICE_HOST'])->notEmpty();
+
+} catch (\Exception $e) {
+
+    echo "<pre>";
+    print_r($e->getMessage());
+    echo PHP_EOL;
+    echo "</pre>";
 
 }
-
-$dotenv->required(['AITS_STUDENT_ENROLLMENT_APP_DATA_LOG_RELATIVE_PATH', 'AITS_STUDENT_ENROLLMENT_DATA_LOG_FILE_PREFIX', 'AITS_STUDENT_ENROLLMENT_AITS_SENDER_APP_ID', 'AITS_STUDENT_ENROLLMENT_AITS_SERVICE_HOST'])->notEmpty();
